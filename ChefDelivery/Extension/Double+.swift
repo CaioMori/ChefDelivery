ce547436252b7e545cc4eaffce9c8a6dc859b4ec
Caio Mori <caiomori22@gmail.com>
2024-11-28 13:07:10 -0300

    feat: added product screen layout

--- ChefDelivery/Extension/Double+.swift
+++ ChefDelivery/Extension/Double+.swift
@@ -9,9 +9,8 @@ import Foundation
 
 extension Double {
     func formatPrice () -> String {
-        var formattedString: String = String(format: "%.2f", self).replacingOccurrences(of: ".", with: ",")
+        let formattedString: String = String(format: "%.2f", self).replacingOccurrences(of: ".", with: ",")
 
-        
         return formattedString
     }
 }
