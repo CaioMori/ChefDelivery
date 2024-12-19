03236cd6f0a983a85afe6febce9cbbbfba810c67
Caio Mori <caiomori22@gmail.com>
2024-12-18 22:13:24 -0300

    feat: add timer to change banner item each 3 seconds

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CarouselTabView: View {
     
+    @State private var currentIndex: Int = 1
+    
     let orderMock: [OrderType] = [
         OrderType(id: 1, name: "Churrasco", image: "barbecue-banner"),
         OrderType(id: 2, name: "PF", image: "brazilian-meal-banner"),
@@ -16,13 +18,22 @@ struct CarouselTabView: View {
     ]
     
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(orderMock) { orderItem in
                 CarouselItemView(order: orderItem)
+                    .tag(orderItem.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                if currentIndex >= orderMock.count {
+                    currentIndex = 1
+                }
+                currentIndex += 1
+            }
+        }
     }
 }
 
